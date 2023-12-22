7a494feacd367030bc3041e4c3d32b3794f913be
Tarun Mitruka <mitruka@ibb.uni-stuttgart.de>
2023-12-22 10:54:17 +0000

    First commit

---
+++ config.h.cmake
@@ -0,0 +1,45 @@
+/* begin dune-maxent
+   put the definitions for config.h specific to
+   your project here. Everything above will be
+   overwritten
+*/
+
+/* begin private */
+/* Name of package */
+#define PACKAGE "@DUNE_MOD_NAME@"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "@DUNE_MAINTAINER@"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "@DUNE_MOD_NAME@"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "@DUNE_MOD_NAME@ @DUNE_MOD_VERSION@"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "@DUNE_MOD_NAME@"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "@DUNE_MOD_URL@"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "@DUNE_MOD_VERSION@"
+
+/* end private */
+
+/* Define to the version of dune-maxent */
+#define DUNE_MAXENT_VERSION "@DUNE_MAXENT_VERSION@"
+
+/* Define to the major version of dune-maxent */
+#define DUNE_MAXENT_VERSION_MAJOR @DUNE_MAXENT_VERSION_MAJOR@
+
+/* Define to the minor version of dune-maxent */
+#define DUNE_MAXENT_VERSION_MINOR @DUNE_MAXENT_VERSION_MINOR@
+
+/* Define to the revision of dune-maxent */
+#define DUNE_MAXENT_VERSION_REVISION @DUNE_MAXENT_VERSION_REVISION@
+
+/* end dune-maxent
+   Everything below here will be overwritten
+*/
---
+++ dune/maxent/maxent.hh
@@ -0,0 +1,6 @@
+#ifndef DUNE_MAXENT_HH
+#define DUNE_MAXENT_HH
+
+// add your classes here
+
+#endif // DUNE_MAXENT_HH
